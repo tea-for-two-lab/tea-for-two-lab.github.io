e9ee898d65f4bff792480f83462d323b6e9c1815
tea-for-two-lab <bwaroude@yahoo.fr>
2026-02-04 20:06:48 +0100

    Update include-footer.js

--- assets/js/include-footer.js
+++ assets/js/include-footer.js
@@ -1,1 +1,8 @@
-
+fetch('/assets/includes/footer.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('footer-container').innerHTML = html;
+  })
+  .catch(error => {
+    console.error('Erreur de chargement du footer', error);
+  });
